08acea517328040dae7169a935d58a1a794cc037
akar867 <kararnab99@gmail.com>
2023-11-15 18:57:12 +0530

    first commit

---
+++ Product_Crud_API/src/main/java/com/product/api/model/Product.java
@@ -0,0 +1,27 @@
+package com.product.api.model;
+
+import org.springframework.data.relational.core.mapping.Table;
+
+import com.mysql.cj.x.protobuf.MysqlxDatatypes.Scalar.String;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "PRODUCT_TBL")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
